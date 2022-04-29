267e65e26f6c7d7fba1c73c802d8308f6b084e6e
Antoni Pawlak <antoni.pawlak@protonmail.com>
2022-04-29 11:49:55 +0200

    add aggregateReports without tests yet

---
+++ packages/backend/test/api/controllers/report/aggregateReportsDaily.test.ts
@@ -0,0 +1,9 @@
+import { expect } from "earljs";
+
+import { aggregateReportsDaily } from "../../../../src/api/controllers/report/aggregateReportsDaily";
+
+describe(aggregateReportsDaily.name, () => {
+    it('add tests', async () => {
+        expect(true).toEqual(false)
+    })
+})
