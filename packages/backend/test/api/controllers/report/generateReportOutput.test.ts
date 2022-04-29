c54a1784f509cb26fb09839a30065ccf90d37588
Antoni Pawlak <antoni.pawlak@protonmail.com>
2022-04-29 11:50:17 +0200

    add generateReport without tests yet

---
+++ packages/backend/test/api/controllers/report/generateReportOutput.test.ts
@@ -0,0 +1,10 @@
+import { expect } from "earljs";
+
+import { generateReportOutput } from "../../../../src/api/controllers/report/generateReportOutput";
+
+describe(generateReportOutput.name, () => {
+    it('add tests', async () => {
+        expect(true).toEqual(false)
+    })
+})
+
