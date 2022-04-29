93181de808a6479792608f50036391c75d4083d8
Antoni Pawlak <antoni.pawlak@protonmail.com>
2022-04-29 11:52:28 +0200

    add getMaxAssetInBridge without test yet

---
+++ packages/backend/test/api/controllers/report/filter/getMaxAssetInBridge.test.ts
@@ -0,0 +1,9 @@
+import { expect } from "earljs";
+
+import { getMaxAssetInBridge } from "../../../../../src/api/controllers/report/filter/getMaxAssetsInBridge";
+
+describe(getMaxAssetInBridge.name, () => {
+    it('add tests', async () => {
+        expect(true).toEqual(false)
+    })
+})
